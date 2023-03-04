51f4090a1db843e7ac6f9f5e5c2c5c71325ac06f
GuiSSMartins <guissmartins2013@gmail.com>
2023-03-04 06:13:58 +0000

    TPC3 - código json d)

--- TPC3/d_tpc3.py
+++ TPC3/d_tpc3.py
@@ -1,10 +1,15 @@
 from dados import Dados, parsing
 
+import json
+
 # > d) Converta os 20 primeiros registos num novo ficheiro de output mas em formato **Json**.
 # guardar os registos sob a forma de um dicionário
 def d(dados):
+    # https://pythonexamples.org/python-write-json-to-file/
+    registos20 = dados.registos20 # dicionário
+    jsonString = json.dumps(registos20)
+    jsonFile = open("processos20.json", "w")
+    jsonFile.write(jsonString)
+    jsonFile.close()
 
-    registos20 = dados.registos20
-    
-
-    return 0
+    print("\nCriação do ficheiro 'processos20.json' concluído!\n")
