9cce40128960a48cd48f5f99bef961a6256075cc
GuiSSMartins <guissmartins2013@gmail.com>
2023-03-19 23:59:19 +0000

    TPC5 - concluído

--- TPC5/Maquina.py
+++ TPC5/Maquina.py
@@ -29,8 +29,10 @@ class Maquina:
         if self.q0 == 'LEVANTOU':
             self.q0 = 'POUSOU'
             # Indicar montante a devolver
-            print()
-        else: print('maq: "!!! Não pode levantar !!!"')
+            centimos = int((self.saldo % 1) * 100)
+            euros = int(self.saldo - (centimos * 0.01))
+            print('maq: "saldo: ' + '{:d}'.format(euros) + 'e' + '{:d}'.format(centimos) + 'c; Volte sempre!"')
+        else: print('maq: "!!! Não pode pousar !!!"')
 
     # MOEDA $c, $e  ||   MOEDA <lista de valores>
     def adicionarMoedas(self, string_moeda):
@@ -38,24 +40,38 @@ class Maquina:
 
             # EXEMPLO: MOEDA 10c, 30c, 50c, 2e. (CUIDADO com o ponto final)
             pos_moeda = string_moeda[6:]
-            moedas = pos_moeda.split('.')[0].split(',')
+            moedas = pos_moeda.split('.')[0].split(', ')
 
             # Moedas válidas: 1 cêntimo (1c), 2 cêntimos (2c), 5 cêntimos (5c), 10 cêntimos (10c), 20 cêntimos (20c)
             # 50 cêntimos (50c), 1 euro (1e), 2 euros (2e)
             # ATENÇÃO: se encontrar uma moeda inválida, deve escrever uma mensagem de erro
 
+            novo_saldo = 1
+            string_erro = 'maq: "'
+
             for md in moedas:
                 if 'c' in md:
-
+                    # print(md)
+                    centimos = int(md.split('c')[0])
+                    if centimos in [1, 2, 5, 10, 20, 50]:
+                        self.saldo += centimos * 0.01
+                    else:
+                        string_erro += str(centimos) + 'c - moeda inválida; '
+                        novo_saldo = 0
                 elif 'e' in md: 
-
-            m = re.match(r'', string_moeda)
-
-            self.custo_total += valor
-            self.lista_moedas.append(valor)
-
+                    euros = int(md.split('e')[0])
+                    if euros in [1, 2]:
+                        self.saldo += euros
+                    else:
+                        string_erro += str(euros) + 'e - moeda inválida; '
+                        novo_saldo = 0
         else: print('maq: "Não pode adicionar moedas se ainda não LEVANTOU os auscultadores!"')
-
+        centimos_atual = int((self.saldo % 1) * 100)
+        euros_atual = int(self.saldo - (centimos_atual * 0.01))
+        if novo_saldo:
+            print('maq: "saldo: ' + '{:d}'.format(euros_atual) + 'e' + '{:d}'.format(centimos_atual) + 'c"')
+        else:
+            print(string_erro + 'saldo = ' + '{:d}'.format(euros_atual) + 'e' + '{:d}'.format(centimos_atual) + 'c"')
 
 
 
@@ -71,7 +87,7 @@ class Maquina:
         if self.q0 == 'LEVANTOU':
             # Processar número de telefone
             if string_numero[0:3] == "601" or string_numero[0:3] == "641":
-                print('maq: "Chamada _bloqueada_"')
+                print('maq: "Esse número não é permitido neste telefone. Queira discar novo número!"')
             elif string_numero[0:2] == "00":
                 if self.saldo >= 1.5: 
                     self.saldo -= 1.5
@@ -80,8 +96,12 @@ class Maquina:
                     print('maq: "Saldo INSUFICIENTE! Por favor, adicione mais moedas"')
                     print('maq: "Para chamadas internacionais, tem de ter um saldo igual ou superior a 1,5 euros!"')
             elif string_numero[0] == '2':
-
-            
+                if self.saldo >= 0.25:
+                    self.saldo -= 0.25
+                    print('maq. "Chamada nacional efetuada com sucesso."')
+                else:
+                    print('maq: "Saldo INSUFICIENTE! Por favor, adicione mais moedas"')
+                    print('maq: "Para chamadas nacionais, tem de ter um saldo igual ou superior a 25 cêntimos!"')
             elif string_numero[0:3] == "800":
                 print('maq. "Chamada verde efetuada com sucesso."')
             elif string_numero[0:3] == "808":
@@ -93,22 +113,21 @@ class Maquina:
                     print('maq: "Para chamadas azuis, tem de ter um saldo igual ou superior a 10 cêntimos!"')
             else:
                 print('maq: "Esse número não é permitido neste telefone. Queira discar novo número!"')
-            # Adicionar custo da chamada
-            # self.saldo -= valor
         else:
             print('maq: "Por favor, LEVANTE o auscultador antes de telefonar! "')
-        centimos = (self.saldo % 1) * 100
-        euros = self.saldo - centimos
-        print('maq: "SALDO atual: ' + + str(euros) + 'e' + '{:.2f}'.format(centimos) + 'c"')
-        print('maq: "Não se esqueça de POUSAR o auscultador, caso não deseje fazer mais chamadas!"')
+        centimos = int((self.saldo % 1) * 100)
+        euros = int(self.saldo - (centimos * 0.01))
+        print('maq: "saldo: ' + '{:d}'.format(euros) + 'e' + '{:d}'.format(centimos) + 'c"')
+        print('maq: "Não se esqueça de POUSAR o auscultador, caso não deseje realizar mais chamadas!"')
         
 
     # devolver as moedas
     def abortar(self):
         if self.q0 != 'POUSOU' and self.q0 != 'ABORTOU': # Basta que o programa não esteja num estado final para 
             self.q0 = 'ABORTOU'
-            centimos = (self.saldo % 1) * 100
-            euros = self.saldo - centimos
-            print('maq: "Operação abortada! troco: "' + str(euros) + 'e' + '{:.2f}'.format(centimos) + 'c; Volte sempre!"')
+            centimos = int((self.saldo % 1) * 100)
+            euros = int(self.saldo - (centimos * 0.01))
+            print('maq: "saldo: ' + '{:d}'.format(euros) + 'e' + '{:d}'.format(centimos) + 'c"')
+            print('maq: "Operação abortada! troco: ' + '{:d}'.format(euros) + 'e' + '{:d}'.format(centimos) + 'c; Volte sempre!"')
         else: 
             print('maq: "Não pode abortar!"')
--- TPC5/main.py
+++ TPC5/main.py
@@ -1,12 +1,12 @@
 import re
-import Maquina
+from Maquina import Maquina
 
 
 def main():
 
     maquina = Maquina() # Máquina de Estados
 
-    print("\n\nTPC5 - Processamento de Linguagens - 2023")
+    print("\nTPC5 - Processamento de Linguagens - 2023")
     print("Guilherme Martins - a92847 - LEI\n")
 
     print("ATENÇÃO: O programa só funciona para strings fornecidas válidas!\n")
@@ -21,11 +21,12 @@ def main():
         opcao = input()
         # A vanatgem destes matches é que o início da expressão é que tem de estar correto
         # pode ter erros a seguir, mas são ignorados!
-        m1 = re.match(r'^LEVANTAR', opcao)
-        m2 = re.match(r'^POUSAR', opcao)
-        m3 = re.match(r'^MOEDA(\s+[ec][,\.])+', opcao) # ver estrutura 
-        m4 = re.match(r'T=((00)?\d{9,})', opcao) # o número deve ter 9 dígitos excepto se for iniciado por "00"
-        m5 = re.match(r'^ABORTAR', opcao)
+        m1 = re.match(r'^LEVANTAR$', opcao)
+        m2 = re.match(r'^POUSAR$', opcao)
+        m3 = re.match(r'^MOEDA(\s+\d+[e|c],)*\s+\d+[e|c]\.$', opcao) # ver estrutura: MOEDA 10c, 30c, 50c, 2e.
+        m4_1 = re.match(r'^T=(00\d+)$', opcao) # o número deve ter 9 dígitos excepto se for iniciado por "00"
+        m4_2 = re.match(r'T=(\d{9})$', opcao)
+        m5 = re.match(r'^ABORTAR$', opcao) 
 
         if m1: maquina.levantar()
         elif m2: 
@@ -33,16 +34,18 @@ def main():
             perguntar = 0
         elif m3: 
             maquina.adicionarMoedas(opcao)
-        elif m4:
-            maquina.t(m4[1])
+        elif m4_1:
+            maquina.t(m4_1[1])
+        elif m4_2:
+            maquina.t(m4_2[1])
         elif m5:
             maquina.abortar()
             perguntar = 0
         else:
-            print("\n !!! Opção errada !!!")
-            print("maq: (Por favor escreva uma das seguintes opções: )")
+            print('!!! Opção errada !!!')
+            print('maq: "!!! Opção errada !!! (Por favor escreva uma das seguintes opções: )')
             print("-> LEVANTAR; POUSAR; MOEDA $c, $e. (tem de ter um ponto no final);")
-            print("-> T=<o número deve ter 9 dígitos excepto se for iniciado por '0'>; ABORTAR")
+            print('-> T=<o número deve ter 9 dígitos excepto se for iniciado por \'00\'>; ABORTAR"')
     print("\nFIM do programa\n")
 
 if __name__ == '__main__':
